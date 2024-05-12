099db34e30b6b05c3b39635bda1a6b115798bd6d
jogeswarnanda <44108715+jogeswarnanda@users.noreply.github.com>
2024-05-12 12:05:30 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, session
+from database import add_user_to_db, validate_username, verify_login
+
+#Name Initialization
+app = Flask(__name__)
+
+#Home route
+@app.route("/")
+def landing_page():
+  if 'username' in session:
+    return render_template('home.html', username=session['username'])
+  else:
+    return render_template("home.html")
+
+#Login route
+@app.route("/login/", methods=['GET', 'POST'])
+def login_page():
+  if request.method == 'POST':
+    data = []
+    data = request.form
+    lusername = request.form.get('lusername')
+    lpassword = request.form.get('lpassword')
+    found_stat = 'N'
+    pwd = " "
+    found_stat, pwd = verify_login(lusername, data, found_stat, pwd)
+    if found_stat == 'Y':
+      if (lpassword == pwd):
+        session['username'] = lusername
+        username = session['username']
+        return render_template("home.html", username=username)
+      else:
+        return render_template("login.html",
+                               error="Invalid username or password")
+    else:
+      return render_template("login.html",
+                             error="Invalid username or password")
+  else:
+    return render_template("login.html")
+
+#Logout route
+@app.route("/logout/", methods=['GET', 'POST'])
+def logout_page():
+  print("LOGOUT")
+  session.pop('username', None)
+  session.clear()
+  return redirect(url_for('login_page'))
+  #return redirect(url_for('home'))
+
+#Register route
+                  
+@app.route("/register/")
+def register_page():
+  print("Control1 here.....")
+  return render_template("register.html")
+
+#Form submitted route
+@app.route("/register/submitted", methods=['GET', 'POST'])
+def get_user_details():
+  if request.method == 'POST':
+    data = []
+    data = request.form
+    print("POST DATA:", data)
+    user_name = data.get("username")
+    print("before check user name:", user_name)
+    if not data:
+      return "Not Found", 404
+    else:
+      insert_status = 'N'
+      pwd = " "
+      print("insert_status B:", insert_status)
+      insert_status = validate_username(user_name, data, insert_status, pwd)
+      print("insert_status A:", insert_status)
+      return render_template("registration_submitted.html",
+                             data=data,
+                             insert_status_s=insert_status,
+                             pwd=pwd)
+
+
+print(__name__)
+if (__name__ == "__main__"):
+  app.secret_key = 'super secret key'
+  app.config['SESSION_TYPE'] = 'filesystem'
+  app.run(host="0.0.0.0", port=8080, debug=True)
---
+++ database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, text,Table
+import os
+from sqlalchemy.sql.selectable import Values
+from sqlalchemy.orm import scoped_session, sessionmaker
+import psycopg2
+
+db_conn_string = os.environ['DB_CONNECTION_STRING']
+from sqlalchemy import create_engine
+engine=create_engine(db_conn_string)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
+  
+def validate_username(user_name,data,insert_status,pwd):
+    print("username entered by user:", user_name)
+    pwd= " "
+    with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM users where user_name = :username"),
+             {"username" : user_name});
+      rows = result.all()
+      print("rows_new::", rows)
+      if len(rows) == 0:
+        add_user_to_db(user_name,data)
+        insert_status = 'Y'
+        return insert_status,pwd
+      else:
+        print("danger:")
+        insert_status = 'N'
+        return insert_status,pwd
+
+def verify_login (user_name,data,found_stat,pwd):
+  print("username entered by user:", user_name)
+  pwd= " "
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM users where user_name = :username"),
+           {"username" : user_name});
+    rows = result.all()
+    print(type(rows))
+    print("rows_new@@::", rows)
+    print(rows[0][4])
+    if len(rows) == 0:
+      found_stat = "N"
+      print("set found sta N:")
+      return found_stat,pwd
+    else:
+      print("set found stat Y")
+      found_stat = "Y"
+      pwd=rows[0][4]
+      return found_stat,pwd
+      
+def add_user_to_db(user_name,data):
+  with engine.connect() as conn:
+    result1 = conn.execute(text("INSERT INTO users (user_name,user_full_name,user_email,user_password) VALUES(:u_name,:full_name,:email,:password)"),
+{"u_name": data.get("username"), "full_name" : data.get("full_name"),"email" : data.get("email"),"password" : data.get("password")});
+    conn.commit()
